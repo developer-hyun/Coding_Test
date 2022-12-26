c3dfed56094f7b5843f95c78350ddaadaf50f757
hyun <rlckdrkdud@naver.com>
2022-12-26 20:05:59 +0900

    Greedy_until_Number_One_1226

---
+++ Greedy/until_Number_One - 복사본.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class until_Number_One {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+        int result=0;//결과값
+
+    }
+}
