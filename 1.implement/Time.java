4eb4abfb874381ccedc99fbf4b15f997d0cabcd1
hyun <rlckdrkdud@naver.com>
2023-05-03 10:52:47 +0900

    0503_implement_TIME_01

--- 1.implement/Time.java
+++ 1.implement/Time.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
+
 public class Time {
 
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int N  = input.nextInt();
+        int Count = 0;
+
+        for (int i=0;i<N+1;i++){
+            for (int j=0;j<60;j++){
+                for(int k=0;k<60;k++){
+                    if (String.valueOf(i).contains("3") || String.valueOf(j).contains("3") || String.valueOf(k).contains("3")) {
+                        Count += 1;
+                    }
+                }
+            }
+        }
+        System.out.println(Count);
+    }
+
+
 }
