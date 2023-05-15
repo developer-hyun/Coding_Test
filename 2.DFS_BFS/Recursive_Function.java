7265c7af6e1353d908dad312cd02acb7703eebee
hyun <rlckdrkdud@naver.com>
2023-05-15 15:10:20 +0900

    0515_DFSBFS_RECURSIVEFUNCTION_01

---
+++ 2.DFS_BFS/Recursive_Function.java
@@ -0,0 +1,12 @@
+public class Recursive_Function {
+
+    public static void recurisve_function() {
+        System.out.println("재귀함수");
+        recurisve_function();
+    }
+    public static void main(String[] args) {
+        recurisve_function();
+
+
+    }
+}
