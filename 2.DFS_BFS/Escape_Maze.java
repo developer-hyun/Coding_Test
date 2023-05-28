8247d9bb9ac72ca50eb3c842bba6e5fc9b387f21
hyun <rlckdrkdud@naver.com>
2023-05-28 14:02:15 +0900

    0528_DFSBFS_ESCAPEMAZE_02

--- 2.DFS_BFS/Escape_Maze.java
+++ 2.DFS_BFS/Escape_Maze.java
@@ -1,7 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
+
+class Node1{
+    public int x;
+    public int y;
+    public Node1(int x,int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+    public int getY(){
+        return this.y;
+    }
+
+}
+
 
 public class Escape_Maze {
 
@@ -13,9 +33,35 @@ public class Escape_Maze {
 
     public static int N,M;
     public static int[][] graph = new int[100][100];
+    public static int[] dx = {-1,1,0,0};
+    public static int[] dy = {0,0,-1,1};
+
 
     public static int bfs(int x,int y) {
+        Queue<Node1> q = new LinkedList<>();
+        q.offer(new Node1(x,y));
 
+        while (!q.isEmpty()){
+            Node1 node1 = q.poll();
+            int getx = node1.getX();
+            int gety = node1.getY();
+
+            for (int i=0; i<4 ; i++){
+                int nx = getx + dx[i];
+                int ny = gety + dy[i];
+
+                if ( nx < 0 || ny < 0 || nx >=N || ny >= M) {
+                    continue;
+                }
+                //벽
+                if (graph[nx][ny] == 0) {
+                    continue;
+                }
+                if (graph[nx][ny] == 1) {
+                    
+                }
+            }
+        }
     }
 
 
@@ -26,6 +72,8 @@ public class Escape_Maze {
         StringTokenizer st = new StringTokenizer(bf.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+
+
 
         for ( int i=0 ; i<N;i++){
             String[] text = bf.readLine().split("");
