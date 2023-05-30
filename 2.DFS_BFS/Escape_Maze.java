654dd4f1c4f3b3f7d6ecb55d7ae1064679fa9cfd
hyun <rlckdrkdud@naver.com>
2023-05-30 15:53:22 +0900

    0530_DFSBFS_ESCAPEMAZE_06

--- 2.DFS_BFS/Escape_Maze.java
+++ 2.DFS_BFS/Escape_Maze.java
@@ -40,28 +40,21 @@ public class Escape_Maze {
     public static int bfs(int x, int y) {
         Queue<Node1> q = new LinkedList<>();
         q.offer(new Node1(x, y));
-
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()) {
             Node1 node1 = q.poll();
             x = node1.getX();
             y = node1.getY();
-            // 현재 위치에서 4가지 방향으로의 위치 확인
-            for (int i = 0; i < 4; i++) {
+            for ( int i=0 ; i<4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                // 미로 찾기 공간을 벗어난 경우 무시
                 if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-                // 벽인 경우 무시
-                if (graph[nx][ny] == 0) continue;
-                // 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
-                if (graph[nx][ny] == 1) {
+                if (graph[nx][ny]==1){
                     graph[nx][ny] = graph[x][y] + 1;
                     q.offer(new Node1(nx, ny));
                 }
             }
-
         }
-        return graph[N][M];
+        return graph[N-1][M-1];
     }
 
 
@@ -82,7 +75,7 @@ public class Escape_Maze {
             }
         }
 
-        System.out.println(bfs(1,1));
+        System.out.println(bfs(0,0));
 
         int result =0;
 
