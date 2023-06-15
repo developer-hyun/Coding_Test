a61d85bf6cf5827ff6d6de9d526a39e1a2386d9f
hyun <rlckdrkdud@naver.com>
2023-06-15 12:16:43 +0900

    0615_DFSBFS_COUNTSORT_01

---
+++ 3.SORT/Count_Sort.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Count_Sort {
+
+    public static int[] arr = {7,5,9,0,3,1,6,2,9,1,4,8,0,5,2};
+    public static int[] empt = new int[arr.length+1];
+    public static void main(String[] args) {
+
+        for ( int i=0 ; i<arr.length ; i++) {
+            empt[arr[i]] +=1;
+        }
+
+        for ( int j=0; j<empt.length ; j++) {
+            for ( int k=0 ; k < empt[j] ; k++) {
+                System.out.println(j + " ");
+            }
+        }
+
+        
+
+    }
+}
