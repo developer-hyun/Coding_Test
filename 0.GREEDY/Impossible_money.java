643b91e3f79f41e59c4ea89c59003911920304da
hyun <rlckdrkdud@naver.com>
2023-12-01 17:54:53 +0900

    1201_GREEDY_Impossible_Money

--- 0.GREEDY/Impossible_money.java
+++ 0.GREEDY/Impossible_money.java
@@ -13,7 +13,7 @@ public class Impossible_money {
 
     //N개의 동전을 이용하여 만들수 없는 양의 정수 금액 중 최솟값을 구하시오
     //N은 1~ 1000  -> O(N3)
-    //
+    //쉽게말해 기존타켓은 이미 달성했음으로 논외로 치며  거기에 전 코인의값을 더했을 경우가 만들 수 있는 최대의 양의정수 값인데 해당 수가 현재코인의 값보다 작을 경우에는 다 더해도 현재 코인의 값 미만의 수를 만들 수 없다.
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int N  = Integer.parseInt(bf.readLine());
