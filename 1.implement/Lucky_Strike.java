43924b2e320c8b2ae4447df6100d4c2d0b60d127
hyun <rlckdrkdud@naver.com>
2024-01-17 17:36:04 +0900

    0117_implement_Lucky_Strike_01

---
+++ 1.implement/Lucky_Strike.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Lucky_Strike {
+
+    //문제
+    //현재 캐릭터의 점수 N
+    //자리수를 기준으로 N을 반으로 나누어 왼쪽부분의 자리수 합과 오른쪽 부분의 자리수 합을 더한 값이 동일한 상황
+    //ex)) 123,402  면 1+2+3  4+2  는 같음.
+    //N이 주어지면 럭키 스트레이트를 사용할 수 있는 상태인지 아닌지를 알려준다.
+    //N은 10 ~ 99,999,999  O(N) ~ O(logN)
+    //럭키 스트레이트면 LUCKY 없으면 READY
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+       // int N =Integer.parseInt(bf.readLine());
+        String N = bf.readLine();
+        int length = N.length();
+        System.out.println(length);
+
+        for(int i=0 ; i<length; i++) {  //charAt - '0'
+
+
+        }
+
+
+
+    }
+}
