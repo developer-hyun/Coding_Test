5cb35bec6cb1094482543a749113c02bb03bc347
hyun <rlckdrkdud@naver.com>
2024-02-16 19:48:55 +0900

    0216_implement_Snake_02

--- 1.implement/Snake.java
+++ 1.implement/Snake.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Snake {
 
@@ -30,90 +27,67 @@ public class Snake {
     //15 L  //15초뒤 왼쪽
     //17 D //17초뒤 오른쪽
 
-    public static int[][] map = new int[101][101]; //전체 맵
-    public static int N,K;
-    public static int L;
-  //  public static int[][] direction = new int[101][101];
-    public static ArrayList<Snake_Node> direction = new ArrayList<>(); //3초 d와 같은 방향 정보 저장
-    //상하좌우로 움직일때 바뀌는 x,y좌표를 미리 저장해두어 계산한다. 동쪽먼저(동쪽으로 움직이기때문)
-    public static int[] dx = {0 , 1 , 0 ,-1} ;
-    public static int[] dy = {1, 0 , -1, 0};
 
-    public static void main(String[] args) throws IOException {
+    public static int[][] map = new int[101][101]; //맵의 전체 크기
+    public static int N,K,L; //보드의 크기, 사과의 개수 , 방향전환개수
+    public static ArrayList<Snake_Change_Node> Change_Direction = new ArrayList<>(); //3초 d와 같은 정보를 저장하는 배열
+    public static int[] move_x = {0,1,0,-1}; //뱀이 동쪽을 시작으로 움직인다. 움직일때 방향에 따라 x,y좌표가 이동된다
+    public static int[] move_y = {1,0,-1,0};
+
+
+    public static void main(String[] args) throws IOException{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(bf.readLine()); //보드
         K = Integer.parseInt(bf.readLine()); //사과개수
 
-        int[] apple = new int[K];
+
         for(int i=0 ; i<K; i++) {
             StringTokenizer st= new StringTokenizer(bf.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            map[a][b] = 1;
+            map[a][b] = 1; //map에서 1은 사과의 개수다.
         }
 
-        //L입력
         L = Integer.parseInt(bf.readLine());
         for( int i=0 ; i<L ; i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
             int x = Integer.parseInt(st.nextToken());
             char c = st.nextToken().charAt(0);  //direction이라는 배열을 만들어서 넣어보려고하였으나 생각해보니 하나는 int고 하나는 char여서
             //따로 Snake_Node를 만들어서 넣는다.
-            direction.add(new Snake_Node(x,c));
+            Change_Direction.add(new Snake_Change_Node(x,c));
         }
 
-        //사과정보랑 회전정보는 저장!
-        int head_x = 1;
-        int head_y = 1; //시작 좌표 및 뱀의 머리 위치
-        map[head_x][head_y] =2; //뱀이있는위치는 2로 저장
+        int head_x = 1; //뱀의 머리위치
+        int head_y=1; //뱀의 머리위치
+        map[head_x][head_y]=2; //뱀이있는 위치는 2
 
-        int time =0 ; //초를 저장 시간
-        int direction = 0; //뱀이 바라보고있는 방향
+        //뱀이있는 현재 위치를 저장할 배열이 필요하다. 뱀을 담는 배열에서 먼저들어온곳이 먼저 빠져나가야하기때문에 우선순위큐가 필요하다. x,y좌표를 담아야하기 때문에 새로운 노드도 필요
+        Queue<Snake_Location_Node> q = new LinkedList<>();
+        q.offer(new Snake_Location_Node(head_x,head_y));
 
-        //뱀의 위치정보가 필요하다.(차지하고있는) //움직일때 꼬리좌표가없어지고 머리좌표가 추가되기때문에 큐가유리.
-        Queue<Position> q = new LinkedList<>();
-        q.offer(new Position(head_x,head_y));
+        //동서남북을 나타내는 숫자 변수가 필요하다
+        int num_direction=0;
+
+
+
 
         while (true) {
-            int nx = head_x + dx[direction]; //이동은 맨처음에 동쪽으로한다. 이동할 x좌표
-            int ny = head_y + dy[direction]; //이동은 맨처음에 동쪽으로한다. 이동할 y좌표
-            if(nx >=1 && nx <=N && ny>=1 && ny <=N && map[nx][ny] !=2)  {   //nx좌표는 1 ~ N까지 ny좌표는 1 ~ N까지 (보드크기)  , 뱀과부딪치면안됨
+            //뱀이 이동을 하기때문에 이동에 필요한 x,y좌표가 필요하다.
+            int nx = head_x + move_x[num_direction];
+            int ny = head_y + move_y[num_direction];
 
-            }
+            if()
 
         }
-
-
-
-
-
 
     }
 
-    //회전 방향 반환함수
-    public static int turn(int direction , char c) {
-        if ( c == 'L') {
-
-            if ( direction ==0 ) {
-                direction =3;
-            }
-            else {
-                direction -=1;
-            }
-        }
-        else {
-            direction = (direction +1) %4 ;
-        }
-        return direction;
-    }
 }
-
-class Snake_Node {
-
+class Snake_Change_Node {
     private int time;
     private char direction;
 
-    public Snake_Node(int time,char direction ) {
+    public Snake_Change_Node(int time,char direction) {
         this.time = time;
         this.direction = direction;
     }
@@ -121,28 +95,168 @@ class Snake_Node {
     public int getTime() {
         return this.time;
     }
+
     public char getDirection() {
-        return this.direction;
+        return direction;
     }
 }
 
-class Position {
-
+class Snake_Location_Node {
     private int x;
-    private int y;
+    private  int y;
 
-    public Position(int x, int y) {
+    public Snake_Location_Node(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
     public int getX() {
-        return this.x;
+        return x;
     }
 
     public int getY() {
-        return this.y;
+        return y;
     }
 }
 
+//    public static int[][] map = new int[101][101]; //전체 맵
+//    public static int N,K;
+//    public static int L;
+//  //  public static int[][] direction = new int[101][101];
+//    public static ArrayList<Snake_Node> direction = new ArrayList<>(); //3초 d와 같은 방향 정보 저장
+//    //상하좌우로 움직일때 바뀌는 x,y좌표를 미리 저장해두어 계산한다. 동쪽먼저(동쪽으로 움직이기때문)
+//    public static int[] dx = {0 , 1 , 0 ,-1} ;
+//    public static int[] dy = {1, 0 , -1, 0};
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//        N = Integer.parseInt(bf.readLine()); //보드
+//        K = Integer.parseInt(bf.readLine()); //사과개수
+//
+//        int[] apple = new int[K];
+//        for(int i=0 ; i<K; i++) {
+//            StringTokenizer st= new StringTokenizer(bf.readLine());
+//            int a = Integer.parseInt(st.nextToken());
+//            int b = Integer.parseInt(st.nextToken());
+//            map[a][b] = 1;
+//        }
+//
+//        //L입력
+//        L = Integer.parseInt(bf.readLine());
+//        for( int i=0 ; i<L ; i++) {
+//            StringTokenizer st = new StringTokenizer(bf.readLine());
+//            int x = Integer.parseInt(st.nextToken());
+//            char c = st.nextToken().charAt(0);  //direction이라는 배열을 만들어서 넣어보려고하였으나 생각해보니 하나는 int고 하나는 char여서
+//            //따로 Snake_Node를 만들어서 넣는다.
+//            direction.add(new Snake_Node(x,c));
+//        }
+//
+//        //사과정보랑 회전정보는 저장!
+//        int head_x = 1;
+//        int head_y = 1; //시작 좌표 및 뱀의 머리 위치
+//        map[head_x][head_y] =2; //뱀이있는위치는 2로 저장
+//
+//        int time =0 ; //초를 저장 시간
+//        int direction = 0; //뱀이 바라보고있는 방향
+//
+//        //뱀의 위치정보가 필요하다.(차지하고있는) //움직일때 꼬리좌표가없어지고 머리좌표가 추가되기때문에 큐가유리.
+//        Queue<Position> q = new LinkedList<>();
+//        q.offer(new Position(head_x,head_y));
+//
+//        while (true) {
+//            int nx = head_x + dx[direction]; //이동은 맨처음에 동쪽으로한다. 이동할 x좌표
+//            int ny = head_y + dy[direction]; //이동은 맨처음에 동쪽으로한다. 이동할 y좌표
+//            if(nx >=1 && nx <=N && ny>=1 && ny <=N && map[nx][ny] !=2)  {   //nx좌표는 1 ~ N까지 ny좌표는 1 ~ N까지 (보드크기)  , 뱀과부딪치면안됨
+//
+//                //사과가 없으면 꼬리를 줄인다 한칸 전진한다
+//                if(map[nx][ny] == 0 ) {
+//                    map[nx][ny] =2 ; //뱀 한칸전진
+//                    q.offer(new Position(nx,ny)); //전진한 뱀의 위치를 저장한다
+//                    Position pre_pos = q.poll();  //꼬리를 줄였음으로 꼬리를 뺀다
+//                    map[pre_pos.getX()][pre_pos.getY()] =0; //꼬리를 줄였음으로 map에서 뱀을 뺀다
+//                }
+//
+//                //사과가 있으면 사과를 먹고 꼬리가 그대로이다.
+//                if(map[nx][ny] ==1) {
+//                    map[nx][ny]=2; //뱀 한칸 전진
+//                    q.offer(new Position(nx,ny)); //전진한 뱀의 위치를 저장한다
+//                }
+//
+//            }
+//            //뱀이 밖으로 나가거나 벽에 부딪히거나  몸통에 부딪힌다면
+//            else {
+//                time +=1;
+//                break; //끝
+//            }
+//
+//            head_x = nx; //현재 머리의 위치
+//            head_y = ny;
+//            time +=1;
+//
+//            if()
+//
+//        }
+//
+//
+//
+//
+//
+//
+//    }
+//
+//    //회전 방향 반환함수
+//    public static int turn(int direction , char c) {
+//        if ( c == 'L') {
+//
+//            if ( direction ==0 ) {
+//                direction =3;
+//            }
+//            else {
+//                direction -=1;
+//            }
+//        }
+//        else {
+//            direction = (direction +1) %4 ;
+//        }
+//        return direction;
+//    }
+//}
+//
+//class Snake_Node {
+//
+//    private int time;
+//    private char direction;
+//
+//    public Snake_Node(int time,char direction ) {
+//        this.time = time;
+//        this.direction = direction;
+//    }
+//
+//    public int getTime() {
+//        return this.time;
+//    }
+//    public char getDirection() {
+//        return this.direction;
+//    }
+//}
+//
+//class Position {
+//
+//    private int x;
+//    private int y;
+//
+//    public Position(int x, int y) {
+//        this.x = x;
+//        this.y = y;
+//    }
+//
+//    public int getX() {
+//        return this.x;
+//    }
+//
+//    public int getY() {
+//        return this.y;
+//    }
+//}
+//
 
