cb2162b6393a4d50ea6b42c95d06eb543e2dbd1f
hyun <rlckdrkdud@naver.com>
2024-09-20 15:47:50 +0900

    24.09.20_Input_Back2557_HelloWorld

---
+++ 6.String,InputOutput/BAEKJOON_2557_HelloWorld.java
@@ -0,0 +1,9 @@
+import java.lang.System;
+
+public class BAEKJOON_2557_HelloWorld {
+    //백준 2557 Hello World
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ 6.String,InputOutput/Jumongs_Order.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
+public class Jumongs_Order {
+
+    //1. 주몽이 갑옷을 만들라는 명령을 내렸다. 재료들은 고유의 번호가 있고 갑옷은 2가지 재료의 교유번호를 합쳐 M이되면 갑옷이된다.
+    // N개의 재료와 M이 주어졌을때 몇개의 갑옷을 만들수 있는지 구하여라.
+    //M은 1 ~ 10,000,000  N은 1 ~ 15,000
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        int M = Integer.parseInt(bf.readLine());
+        int[] num = new int[N];
+
+
+
+
+
+    }
+}
