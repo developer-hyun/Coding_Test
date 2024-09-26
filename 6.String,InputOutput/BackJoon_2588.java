97ff5f48300d68d5688639c76e4493d1be562977
hyun <rlckdrkdud@naver.com>
2024-09-26 17:47:51 +0900

    24_09_26_Input_Back_2588

--- 6.String,InputOutput/BackJoon_2588.java
+++ 6.String,InputOutput/BackJoon_2588.java
@@ -1,3 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BackJoon_2588 {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf =  new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        int M = Integer.parseInt(bf.readLine());
+        System.out.println((M%10)*N);
+        System.out.println((M%100)/10*N);
+        System.out.println((M/100)*N);
+        System.out.println(N*M);
+    }
 }
