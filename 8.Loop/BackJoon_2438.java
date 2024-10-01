4c3aa3609e7d689eef4c47e278b24243cd199786
hyun <rlckdrkdud@naver.com>
2024-10-01 16:46:37 +0900

    24_10_01_Loop_Back_15552_2438

---
+++ 8.Loop/BackJoon_2438.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BackJoon_2438 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N =  Integer.parseInt(bf.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=1 ; i<=N ; i++) {
+            for(int j=1; j<=i;j++) {
+                sb.append("*");
+            }
+            System.out.println(sb);
+            sb.setLength(0);
+        }
+    }
+}
